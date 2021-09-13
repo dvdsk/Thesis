1818ade7fe5ab7ca323810fadc981e0c024a3e01
dskleingeld <11743287+dskleingeld@users.noreply.github.com>
2021-09-13 16:45:59 +0200

    adds jeager startup scripts

--- tracing/open_dashboard.sh
+++ tracing/open_dashboard.sh
@@ -1,10 +1,3 @@
 #!/usr/bin/env bash
 
-echo opening ssh to cluster frontend binding to port 8080
-
-cmd="ssh -D 8080 dpsdas \"mock-fs/tracing/jeager-all-in-one\"; sleep 90"
-tmux kill-session -t "tracing" 
-tmux new-session -s "tracing" -n "tracing" -d "$cmd"
-
-sleep 10
 firefox http://127.0.0.1:16686
