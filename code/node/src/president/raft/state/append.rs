4303130f745ef5b45842e7f03c5d9a467ab71f5c
dvdsk <noreply@davidsk.dev>
2022-05-12 14:36:58 +0200

    fix increment fn, implement prepare log

--- code/node/src/president/raft/state/append.rs
+++ code/node/src/president/raft/state/append.rs
@@ -8,49 +8,51 @@ use crate::{Id, Term};
 
 impl State {
     #[instrument(skip(self), fields(id = self.id), ret)]
-    pub fn append_req(&self, req: Request) -> Reply {
-        let mut election_office = self.election_office.lock().unwrap();
-        let election_data = election_office.data();
-        let mut term = election_data.term();
+    pub async fn append_req(&self, req: Request) -> Reply {
+        {
+            // lock scope of election_office
+            let mut election_office = self.election_office.lock().unwrap();
+            let election_data = election_office.data();
+            let mut term = election_data.term();
 
-        if req.term > *term {
-            election_office.set_term(req.term); // needs lock
-            term = &req.term;
-        }
+            if req.term > *term {
+                election_office.set_term(req.term); // needs lock
+                term = &req.term;
+            }
 
-        if req.term < *term {
-            return Reply::ExPresident(*term);
-        }
+            if req.term < *term {
+                return Reply::ExPresident(*term);
+            }
 
-        // got rpc from current leader
-        self.vars.heartbeat.notify_one();
+            // at this point the request can only be from 
+            // (as seen from this node) a valid leader
+            self.vars.heartbeat.notify_one();
 
-        if !self.log_contains(req.prev_log_idx, req.prev_log_term) {
-            return Reply::InconsistentLog;
-        }
+            // From here on we need to be locked , are locked by the election_office
+            if !self.log_contains(req.prev_log_idx, req.prev_log_term) {
+                return Reply::InconsistentLog;
+            }
 
-        // This must execute in parallel without side effects
-        // see called functions for motivation
-        let n_entries = req.entries.len() as u32;
-        for (i, order) in req.entries.into_iter().enumerate() {
-            let index = req.prev_log_idx + i as u32 + 1;
-            self.prepare_log(index, req.term);
-            let entry = LogEntry { term: *term, order };
-            self.insert_into_log(index, &entry)
-        }
+            let n_entries = req.entries.len() as u32;
+            for (i, order) in req.entries.into_iter().enumerate() {
+                let index = req.prev_log_idx + i as u32 + 1;
+                self.prepare_log(index, req.term);
+                let entry = LogEntry { term: *term, order };
+                self.insert_into_log(index, &entry)
+            }
 
-        if req.leader_commit > self.commit_index() {
-            let last_new_idx = req.prev_log_idx + n_entries;
-            let new = u32::min(req.leader_commit, last_new_idx);
-            self.set_commit_index(new);
-        }
+            if req.leader_commit > self.commit_index() {
+                let last_new_idx = req.prev_log_idx + n_entries;
+                let new = u32::min(req.leader_commit, last_new_idx);
+                self.set_commit_index(new);
+            }
+        } // end lock scope
 
-        // This must execute in parallel without side effects
-        // see called functions for motivation
+        // safely runs concurrently; TODO proof
         let last_applied = self.last_applied();
         if self.commit_index() > last_applied {
             let to_apply = self.increment_last_applied();
-            self.apply_log(to_apply);
+            self.apply_log(to_apply).await;
         }
 
         Reply::Ok
@@ -73,6 +75,7 @@ impl Default for LogEntry {
 }
 
 impl State {
+    /// return true if the log contains prev_log_idx and prev_log_texm
     pub(super) fn log_contains(&self, prev_log_idx: u32, prev_log_term: u32) -> bool {
         match self.db.get_val(db::log_key(prev_log_idx)) {
             Some(LogEntry { term, .. }) if term == prev_log_term => true,
@@ -80,20 +83,40 @@ impl State {
         }
     }
 
-    // check side effects if called interleaved
-    fn prepare_log(&self, _index: u32, _term: u32) {
-        todo!()
+    // TODO check side effects if called interleaved
+    // If an existing entry conflicts with a new one (same index
+    // but different terms), delete the existing entry and all that follow it
+    fn prepare_log(&self, index: u32, term: u32) {
+        let existing_entry = self.db.get_val(db::log_key(index));
+        let existing_term = match existing_entry {
+            None => return,
+            Some(LogEntry { term, .. }) => term,
+        };
+
+        if existing_term != term {
+            for key in self
+                .db
+                .range(db::log_key(index)..)
+                .keys()
+                .map(Result::unwrap)
+            {
+                self.db.remove(key).expect("key should still be present");
+            }
+        }
     }
 
-    pub(super) fn insert_into_log(&self, index: u32, entry: &LogEntry) {
-        let mut key = [db::Prefix::Log as u8, 0, 0, 0, 0];
-        key[1..5].clone_from_slice(&index.to_ne_bytes());
-        self.db.set_val(key, entry);
+    #[instrument(skip(self))]
+    pub(super) fn insert_into_log(&self, idx: u32, entry: &LogEntry) {
+        self.db.set_val(db::log_key(idx), entry);
     }
 
-    pub(super) fn apply_log(&self, _idx: u32) {
-        todo!();
-        //self.order()
+    #[instrument(skip(self))]
+    pub(super) async fn apply_log(&self, idx: u32) {
+        let LogEntry{order, ..} = self
+            .db
+            .get_val(db::log_key(idx))
+            .expect("there should be an item in the log");
+        self.tx.send(order).await.unwrap(); // TODO add backpressure?
     }
 }
 
@@ -108,7 +131,7 @@ impl State {
     }
 
     fn increment_last_applied(&self) -> u32 {
-        self.vars.last_applied.fetch_add(1, Ordering::SeqCst)
+        self.vars.last_applied.fetch_add(1, Ordering::SeqCst) + 1
     }
 
     pub(crate) fn last_applied(&self) -> u32 {
@@ -126,7 +149,7 @@ pub struct Request {
     pub leader_commit: LogIdx,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
 pub enum Reply {
     Ok,
     ExPresident(Term),
