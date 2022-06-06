5d1e9c345313db5f6d6a4397e4a0feadd9d40da0
dvdsk <noreply@davidsk.dev>
2022-06-06 16:22:00 +0200

    idle minister promotion + minister skeleton

--- code/node/src/idle.rs
+++ code/node/src/idle.rs
@@ -2,14 +2,29 @@ use color_eyre::eyre::eyre;
 use color_eyre::{Result, Section, SectionExt};
 
 use crate::president::{Log, Order};
-use crate::Role;
+use crate::{Id, Role};
 
-pub(crate) async fn work(pres_orders: &mut Log) -> Result<Role> {
-    match pres_orders.recv().await {
-        Order::Assigned(role) => Ok(role),
-        Order::BecomePres { term } => Ok(Role::President { term }),
-        m => {
-            Err(eyre!("recieved wrong msg")).with_section(move || format!("{m:?}").header("Msg:"))
+pub(crate) async fn work(pres_orders: &mut Log, id: Id) -> Result<Role> {
+    loop {
+        match pres_orders.recv().await {
+            Order::AssignMinistry { subtree, staff } => {
+                if staff.minister == id {
+                    return Ok(Role::Minister {
+                        subtree,
+                        clerks: staff.clerks,
+                    });
+                }
+
+                if staff.clerks.contains(&id) {
+                    return Ok(Role::Clerk);
+                }
+            }
+            Order::Assigned(role) => return Ok(role),
+            Order::BecomePres { term } => return Ok(Role::President { term }),
+            m => {
+                return Err(eyre!("recieved wrong msg"))
+                    .with_section(move || format!("{m:?}").header("Msg:"))
+            }
         }
     }
 }
--- code/node/src/minister.rs
+++ code/node/src/minister.rs
@@ -1,7 +1,61 @@
+use std::path::PathBuf;
+
+use color_eyre::Result;
+use futures::pin_mut;
 use tokio::net::TcpListener;
 
-use crate::president::Log;
+use crate::president::{Log, Order};
+use crate::{Id, Role};
 
-pub(crate) fn work(_pres_orders: &mut Log, _socket: &mut TcpListener) -> crate::Role {
-    todo!()
+async fn handle_client_requests(socket: &mut TcpListener) {
+    for stream in socket.accept().await {
+        todo!();
+    }
+}
+
+async fn handle_pres_orders(
+    pres_orders: &mut Log,
+    our_id: Id,
+    our_subtree: PathBuf,
+) -> Result<Role> {
+    loop {
+        match pres_orders.recv().await {
+            Order::None => todo!(),
+            Order::Assigned(_) => todo!(),
+            Order::BecomePres { term } => return Ok(Role::President { term }),
+            Order::ResignPres => unreachable!(),
+            Order::AssignMinistry { subtree, staff } => {
+                if staff.minister == our_id && subtree != our_subtree {
+                    return Ok(Role::Minister {
+                        subtree,
+                        clerks: staff.clerks,
+                    });
+                }
+
+                if staff.clerks.contains(&our_id) {
+                    return Ok(Role::Clerk);
+                }
+            }
+            #[cfg(test)]
+            Order::Test(_) => todo!(),
+        }
+    }
+}
+
+pub(crate) async fn work(
+    pres_orders: &mut Log,
+    socket: &mut TcpListener,
+    our_id: Id,
+    our_subtree: PathBuf,
+    clerks: Vec<Id>,
+) -> Result<Role> {
+
+    let ministry = todo!();
+    let client_requests = handle_client_requests(socket);
+    let pres_orders = handle_pres_orders(pres_orders, our_id, our_subtree);
+
+    tokio::select! {
+        new_role = pres_orders => return new_role,
+        _ = client_requests => unreachable!(),
+    };
 }
