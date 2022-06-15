78395575773543c86fcb4a97f246578279f489ed
dvdsk <noreply@davidsk.dev>
2022-06-15 12:51:36 +0200

    directory now uses sled db as backend

--- code/node/src/directory.rs
+++ code/node/src/directory.rs
@@ -1,43 +1,28 @@
 use std::ops::Range;
+use std::os::unix::prelude::OsStrExt;
 use std::path::PathBuf;
-use std::sync::Arc;
 
-use tokio::sync::RwLock;
+use serde::{Deserialize, Serialize};
 
 use crate::raft::State;
 
+#[derive(Serialize, Deserialize)]
 pub enum Access {
     Reader(Range<usize>),
     Writer(Range<usize>),
 }
 
-#[derive(Default)]
+#[derive(Default, Serialize, Deserialize)]
 pub struct File {
-    path: PathBuf,
     size: usize,
     areas: Vec<Access>,
 }
 
-#[derive(Default)]
+#[derive(Default, Serialize, Deserialize)]
 pub struct Folder {
-    path: PathBuf,
     size: usize,
     areas: Vec<Access>,
 }
-
-impl Entry {
-    fn new_file(path: PathBuf) -> Self {
-        Self::File(File {
-            path, .. Default::default()
-        })
-    }
-    fn new_folder(path: PathBuf) -> Self {
-        Self::Folder(Folder {
-            path, .. Default::default()
-        })
-    }
-}
-
 
 pub enum Entry {
     File(File),
@@ -45,25 +30,45 @@ pub enum Entry {
 }
 
 pub struct Directory {
-    tree: Arc<RwLock<Vec<Entry>>>,
+    tree: sled::Tree,
 }
 
 impl Directory {
-    pub fn add_entry(&mut self, entry: Entry) {}
+    pub fn add_entry(&mut self, path: PathBuf) {
+        let entry = match path.is_dir() {
+            true => Folder::default(),
+            false => Folder::default(),
+        };
+        let key = path.as_os_str().as_bytes();
+        let entry = bincode::serialize(&entry).unwrap();
+        self.tree.insert(key, entry).unwrap();
+    }
 
-    pub fn remove_path(&mut self, path: PathBuf) {}
+    pub fn remove_path(&mut self, path: PathBuf) {
+        let key = path.as_os_str().as_bytes();
+        if path.is_file() {
+            self.tree.remove(key);
+            return;
+        }
 
-    pub fn from_committed(state: &State) -> Self {
+        // TODO check if mounted/subdir part
+        for dir_entry in self.tree.scan_prefix(key).keys().map(Result::unwrap) {
+            self.tree.remove(dir_entry);
+        }
+    }
+
+    pub fn from_committed(state: &State, db: sled::Db) -> Self {
         use crate::minister::Order::*;
 
-        let tree = Vec::new();
-        let mut dir = Self {
-            tree: Arc::new(RwLock::new(tree)),
-        };
+        db.drop_tree("directory").unwrap();
+        let tree = db.open_tree("directory").unwrap();
+        let mut dir = Self { tree };
         for order in state.committed() {
-            CreateFile(path) => dir.add_entry(Entry::new_file(path)),
-            CreateFolder(path) => dir.add_entry(Entry::new_folder(path)),
-            Remove(path) => dir.remove_path(path),
+            // TODO make raft generic over order
+            match order {
+                Create(path) => dir.add_entry(path),
+                Remove(path) => dir.remove_path(path),
+            }
         }
 
         dir
