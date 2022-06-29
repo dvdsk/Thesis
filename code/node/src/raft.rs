12cc1255e3ae0aeafc10424d1443ec741bd8d870
dvdsk <noreply@davidsk.dev>
2022-06-29 18:41:02 +0200

    started work on structure diagram

--- code/node/src/raft.rs
+++ code/node/src/raft.rs
@@ -24,7 +24,7 @@ mod tests;
 
 pub use log::{Log, ObserverLog};
 pub use log_writer::LogWriter;
-pub use state::{PerishableOrder, LogEntry, State};
+pub use state::{LogEntry, PerishableOrder, State};
 
 use crate::Term;
 
